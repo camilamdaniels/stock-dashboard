53ee8d804d1fc8447afe0ca2d4613b1e507bfa28
camilamdaniels <camila.daniels926@gmail.com>
2023-09-04 18:46:42 -0400

    Initial commit.

---
+++ src/components/ChartBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChartBox = () => {
+  return (
+    <div>ChartBox</div>
+  )
+}
+
+export default ChartBox
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Navbar />
+        Dashboard
+    </div>
+  )
+}
+
+export default Dashboard
